5d6d1f058af29698aba59aa6495875e20f85750c
Serhii Hulko <3834080+felytic@users.noreply.github.com>
2021-12-01 13:46:23 +0100

    Fix linter errors

--- src/models.py
+++ src/models.py
@@ -1,20 +1,17 @@
-from enum import Enum
 from typing import List, Optional
 
-from pydantic import BaseModel, Field
-
-
-class BlockTypeEnum(str, Enum):
-    data = "DATA"
-    processing = "PROCESSING"
+from pydantic import BaseModel
 
 
 class DataModel(BaseModel):
     ids: Optional[str] = None
-    bbox: list # validate with: shapely.geometry.box(*bbox) using shapely.geometry import box
-    contains: dict # validate with: shape(contains) using shapely.geometry import shape
-    intersects: dict  # validate with: shape(contains) using shapely.geometry import shape
+    # validate with: shapely.geometry.box(*bbox)
+    bbox: list
+    # validate with: shape(contains) using shapely.geometry import shape
+    contains: dict
+    # validate with: shape(contains) using shapely.geometry import shape
+    intersects: dict
     zoom_level: int = 18
-    time_series: Optional[List[str]] = None # validate with datetime package
-    time: str # validate with datetime package
+    time_series: Optional[List[str]] = None  # validate with datetime package
+    time: str  # validate with datetime package
     max_cloud_cover: int = 100
